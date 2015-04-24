0cf92f93a8406c33a1ce215c4bd8afb4ccad23c5
graingerkid <craig.addyman@googlemail.com>
2015-04-24 09:02:50 +0000

    minor changes

--- rank_checker/rank_checker.py
+++ rank_checker/rank_checker.py
@@ -3,7 +3,6 @@ import requests
 import time
 import random
 
-
 pages_deep = 1 # production would be more like 100 pages deep
 
 # from: http://techblog.willshouse.com/2012/01/03/most-common-user-agents/
